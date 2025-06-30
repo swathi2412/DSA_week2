15881413fdace9130bbafb64db67c3ba63bfae05
swathi2412 <swathi.sundar2412@gmail.com>
2025-06-30 21:16:22 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,14 @@
+public class pattern2 {
+    
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+              System.out.print(j);
+            }
+            System.err.println(" ");
+        }
+    }
+}
